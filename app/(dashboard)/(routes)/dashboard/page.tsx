ffd3cd0bb63ed27fc9617e87f8b96500fe900203
Russell Peiris <ardpeiris@gmail.com>
2023-07-28 10:12:01 +0530

    initial

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from '@/components/ui/button'
+
+export default function DashboardPage() {
+  return (
+    <Button>Dash me</Button>
+)}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from '@/components/ui/button'
+
+export default function LandingPage() {
+  return (
+    <Button>Click me</Button>
+)}
